9f8905109fa90941b554b453d08b211892e840fb
Mack Grissom <mcintyregrissom@gmail.com>
2022-08-22 12:45:03 +0700

    added services section

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Design from '../assets/design.png'
+import Development from '../assets/development.png'
+import SEO from '../assets/seo.png'
+
+
+const Analytics = () => {
+    return (
+        <div className='w-full bg-white py-16 px-4'>
+
+<div className='max-w-[800px] mt-[-96px] w-full pt-5 mx-auto text-center flex flex-col justify-center'>
+<h1 className='md:text-6xl sm:text-5xl text-4xl font bold md:py-6 font font-bold text-black '>  </h1>
+</div>
+            <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 shadow-lg'>
+                <img src={Design} alt='laptop' className='w-[500px] mx-auto my-4' />
+                <div className='flex flex-col justify-center'>
+                    <p className='text-[#ffde59] uppercase font-bold shadow-md md:text-4x1 sm:text-3x1'> Web Design  </p>
+                    <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'> Design </h1>
+                    <p> Whether you're seeking a web-re-design, a make-over of your existing website, a mobile optimized version of your site, or a brand new site designed and developed from scratch; you can be confident that you are in good hands with our web design specialists. </p>
+
+                    <button className='text-black  bg-[#ffde59] w-[200px] rounded-md font-medium my-6 py-3 mx-auto md:mx-0'> Get Started </button>
+                </div>
+            </div>
+            <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 shadow-lg'>
+                <img src={Development} alt='laptop' className='w-[500px] mx-auto my-4' />
+                <div className='flex flex-col justify-center'>
+                    <p className='text-[#ffde59] uppercase font-bold shadow-md'> Web Development  </p>
+                    <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'> Build</h1>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eveniet, vel amet nihil placeat minima quisquam nam nemo hic. Accusamus voluptatum mollitia eligendi molestias optio debitis nulla neque eveniet alias. </p>
+
+                    <button className='text-black  bg-[#ffde59] w-[200px] rounded-md font-medium my-6 py-3 mx-auto md:mx-0'> Get Started </button>
+                </div>
+            </div>
+            <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 shadow-lg'>
+                <img src={SEO} alt='laptop' className='w-[500px] mx-auto my-4' />
+                <div className='flex flex-col justify-center'>
+                    <p className='text-[#ffde59] uppercase font-bold shadow-md'> SEO Optimization  </p>
+                    <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'> Optimize </h1>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eveniet, vel amet nihil placeat minima quisquam nam nemo hic. Accusamus voluptatum mollitia eligendi molestias optio debitis nulla neque eveniet alias. </p>
+
+                    <button className='text-black  bg-[#ffde59] w-[200px] rounded-md font-medium my-6 py-3 mx-auto md:mx-0'> Get Started </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Analytics
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,20 +4,22 @@ import Typed from 'react-typed';
 
 const Hero = () => {
     return (
-        <div className='text-white pt-h-14  '>
+        <div className='text-white pt-h-14 '>
             <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
                 {/* <p className='text-[#000000] font-bold p-2'>Design, Develop, Deliver. </p> */}
 
-                <h1 className='md:text-6xl sm:text-5xl text-4xl font bold md:py-6'> From an Idea to a Reality</h1>
+                <h1 className='md:text-6xl sm:text-5xl text-4xl font bold md:py-6'> Leaders In Web Design </h1>
                 <div>
-                    <p className='md:text-3xl sm:text-2xl text-xl font-bold'> We Build & Design Modern, Responsive Websites for </p>
+                    <p className='md:text-3xl sm:text-2xl text-xl font-bold'> We Design, Build & Optimize Modern, Responsive Websites for <Typed strings={['Disruptors.', 'Dreamers.', 'Doers.', 'Innovators.', 'Inventors.', 'Startups.', 'Small Businesses.', "Enterprises.", "Entrepreneurs.", "You."]} typeSpeed={100} backSpeed={50} loop className='font-5xl text-[#ffde59] font-bold md:text-3xl sm:text-2xl text-xl' /></p>
 
-                    <Typed strings={['Disruptors.', 'Dreamers.', 'Doers.', 'Innovators.', 'Inventors.', 'Startups.', 'Small Businesses.', "Enterprises.", "Entrepreneurs.", "You."]} typeSpeed={100} backSpeed={50} loop className='font-5xl text-[#ffde59] font-bold md:text-3xl sm:text-2xl text-xl font-bold' />
+                    
                 </div>
-                <p className='text-white'>
-                   Web Design & Web Development transforming your business 
-                </p>
+                {/* <p className='text-white'>
+                   Our websites will take you from zero to one.
+                </p> */}
+                 <button className='text-black  bg-[#ffde59] w-[200px] rounded-md font-medium my-6 mx-auto py-3'> Get Started</button>
             </div>
+           
         </div>
     )
 }
