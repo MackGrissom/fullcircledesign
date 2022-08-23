90c2aa5679e81766593e905e35cba709c592a6ed
Mack Grissom <mcintyregrissom@gmail.com>
2022-08-23 17:35:42 +0700

    added newletter section

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -14,9 +14,9 @@ const Analytics = () => {
             <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 shadow-lg'>
                 <img src={Design} alt='laptop' className='w-[500px] mx-auto my-4' />
                 <div className='flex flex-col justify-center'>
-                    <p className='text-[#ffde59] uppercase font-bold shadow-md md:text-4x1 sm:text-3x1'> Web Design  </p>
+                    <p className='text-[#ffde59] uppercase font-bold md:text-4x1 sm:text-3x1'> Web Design  </p>
                     <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'> Design </h1>
-                    <p> Whether you're seeking a web-re-design, a make-over of your existing website, a mobile optimized version of your site, or a brand new site designed and developed from scratch; you can be confident that you are in good hands with our web design specialists. </p>
+                    <p> Whether you're seeking a web-re-design, a make-over of your existing website, a mobile-friendly  version of your site, or a brand new site designed and developed from scratch; you can be confident that you are in good hands with our web design specialists. </p>
 
                     <button className='text-black  bg-[#ffde59] w-[200px] rounded-md font-medium my-6 py-3 mx-auto md:mx-0'> Get Started </button>
                 </div>
@@ -24,7 +24,7 @@ const Analytics = () => {
             <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 shadow-lg'>
                 <img src={Development} alt='laptop' className='w-[500px] mx-auto my-4' />
                 <div className='flex flex-col justify-center'>
-                    <p className='text-[#ffde59] uppercase font-bold shadow-md'> Web Development  </p>
+                    <p className='text-[#ffde59] uppercase font-bold'> Web Development  </p>
                     <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'> Build</h1>
                     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eveniet, vel amet nihil placeat minima quisquam nam nemo hic. Accusamus voluptatum mollitia eligendi molestias optio debitis nulla neque eveniet alias. </p>
 
@@ -34,7 +34,7 @@ const Analytics = () => {
             <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 shadow-lg'>
                 <img src={SEO} alt='laptop' className='w-[500px] mx-auto my-4' />
                 <div className='flex flex-col justify-center'>
-                    <p className='text-[#ffde59] uppercase font-bold shadow-md'> SEO Optimization  </p>
+                    <p className='text-[#ffde59] uppercase font-bold3'> SEO Optimization  </p>
                     <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'> Optimize </h1>
                     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eveniet, vel amet nihil placeat minima quisquam nam nemo hic. Accusamus voluptatum mollitia eligendi molestias optio debitis nulla neque eveniet alias. </p>
 
