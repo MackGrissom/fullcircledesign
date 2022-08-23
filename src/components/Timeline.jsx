6a7fe0c0baed33bc1da49d3ee67825ebd35b90ac
Mack Grissom <mcintyregrissom@gmail.com>
2022-08-24 00:51:54 +0700

    timeline addition

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -6,6 +6,9 @@ const Timeline = () => {
         <ol class="relative border-l border-gray-200 dark:border-gray-700 mt-10 ml-5">
             <h1 className='md:text-6xl sm:text-5xl text-4xl font bold md:py-6 text-[#ffde59]'> Our process </h1>
             <p className='ml-5 text-[white]'>  A step-by-step timeline of turning your idea into reality</p>
+           
+           
+           {/* Step 1 */}
             <li class="mb-10 ml-6 mt-10">
                 <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-[#ffde59]">
                     <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-[black]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
@@ -23,7 +26,7 @@ const Timeline = () => {
                 <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
                     <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                 </span>
-                <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white"> <span className='text-[#ffde59]'> YOU </span> <br /> <br /> Submit a brief description of your website needs </h3>
+                <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white"> <span className='text-[#1F3A8A]'> WE </span> <br /> <br /> Provide you with a Sitemap and Low-Fidelity Wireframe in Figma </h3>
 
                 <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 7th, 2021</time>
                 <p class="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
