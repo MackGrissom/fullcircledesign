c471cb0c06e7a08a82a10a10d4d85731fa00a3a0
Mack Grissom <mcintyregrissom@gmail.com>
2022-09-10 19:18:46 +0700

    deleted unecessary logos

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import BG from '../assets/bg.mp4'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import BG2 from '../assets/bg20.mp4'
